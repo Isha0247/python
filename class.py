b89459240a484b69e5fde1961602a3d935d01425
Isha0247 <143619021+Isha0247@users.noreply.github.com>
2023-10-21 10:06:09 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,15 @@
+class Cat:
+    name=''
+    def speak(self):
+        print("Meow! i am {}! Meow".format(self.name))
+     
+myluna = Cat()
+myluna.name="Luna"
+myluna.speak()
+ 
+myBella = Cat()
+myBella.name="Bella"
+myBella.speak()
+
+
+
